f56b9a241ee8be6f0a888aa8eeec671ecec77442
Dimas Salim <40629623+Nekosalim@users.noreply.github.com>
2021-03-18 14:50:04 +0700

    Update Code PWM.ino

--- Code PWM.ino
+++ Code PWM.ino
@@ -1,4 +1,4 @@
-//inisialisasi sensor
+//deklarasi sensor
 int sensor1 = A0;
 int sensor2 = A1;
 int sensor3 = A2;
@@ -6,13 +6,13 @@ int sensor4 = A3;
 int sensor5 = A4;
 int sensor6 = A5;
 int baca_sensor[6];
-//inisialisasi pin output
+//deklarasi pin output
 int pinEnable = 4;
 int pinEnable2 = 2;
-//inisialisasi motor kiri
+//deklarasi motor kiri
 int motor_in1 = 5;
 int motor_in2 = 6;
-//inisialisasi motor kanan
+//deklarasi motor kanan
 int motor_on1 = 3;
 int motor_on2 = 11;
 
@@ -46,7 +46,7 @@ baca_sensor[3] = analogRead(sensor4);
 baca_sensor[4] = analogRead(sensor5);
 baca_sensor[5] = analogRead(sensor6);
 
-delay(500);
+delay(100);
   
   for(int i=0; i<=5; i++){
     Serial.print("Sensor ");
@@ -72,6 +72,7 @@ analogWrite (motor_in1, 0);
 analogWrite (motor_in2, 0);
 analogWrite (motor_on1, 0.5*255);
 analogWrite (motor_on2, 0);
+  error=0;
 }
 
 //jika sensor 2 dan 3 mendeteksi gelap, Duty cycle 20% motor kiri, 50% motor kanan
@@ -84,6 +85,7 @@ analogWrite (motor_in1, 0.2*255);
 analogWrite (motor_in2, 0);
 analogWrite (motor_on1, 0.5*255);
 analogWrite (motor_on2, 0);
+  error=0;
 }
 
 //jika sensor 3 dan 4 mendeteksi gelap, Duty cycle 60% pada kedua motor
@@ -96,6 +98,7 @@ analogWrite (motor_in1, 0.6*255);
 analogWrite (motor_in2, 0);
 analogWrite (motor_on1, 0.6*255);
 analogWrite (motor_on2, 0);
+  error=0;
 }
 
 //jika sensor 4 dan 5, Duty cycle 50% motor kiri, 20% motor kanan
@@ -108,6 +111,7 @@ analogWrite (motor_in1, 0.5*255);
 analogWrite (motor_in2, 0);
 analogWrite (motor_on1, 0.2*255);
 analogWrite (motor_on2, 0);
+  error=0;
 }
 
 //jika sensor 5 dan 6, Duty cycle 50% Motor kiri, 0% motor kanan
@@ -120,9 +124,10 @@ analogWrite (motor_in1, 0.5*255);
 analogWrite (motor_in2, 0);
 analogWrite (motor_on1, 0);
 analogWrite (motor_on2, 0);
+  error=0;
 }
 
-//jika semua sensor mendeteksi gelap, Duty cycle kedua motor 0% (semua motor mati)
+//jika semua sensor mendeteksi terang, Duty cycle kedua motor 0% (semua motor mati)
 if (baca_sensor[0] > 34 && baca_sensor[1] > 34 && 
     baca_sensor[2] > 34 && baca_sensor[3] > 34 && 
     baca_sensor[4] > 34 && baca_sensor[5] > 34){
@@ -132,47 +137,79 @@ analogWrite (motor_in1, 0);
 analogWrite (motor_in2, 0);
 analogWrite (motor_on1, 0);
 analogWrite (motor_on2, 0);
+  error=0;
 }
   
-//jika hanya sensor 1 yang mendeteksi gelap, maka error terdeteksi
+//jika hanya sensor 1 yang mendeteksi gelap, maka error terdeteksi dan menjalankan perintah terdekat
 if (baca_sensor[0] < 34 && baca_sensor[1] > 34 && 
     baca_sensor[2] > 34 && baca_sensor[3] > 34 && 
     baca_sensor[4] > 34 && baca_sensor[5] > 34){
+  digitalWrite (pinEnable, HIGH);
+digitalWrite (pinEnable2, HIGH);
+analogWrite (motor_in1, 0);
+analogWrite (motor_in2, 0);
+analogWrite (motor_on1, 0.5*255);
+analogWrite (motor_on2, 0);
   error = 1;
 }
 
-//jika hanya sensor 2 yang mendeteksi gelap, maka error terdeteksi
+//jika hanya sensor 2 yang mendeteksi gelap, maka error terdeteksi dan menjalankan perintah terdekat
 if (baca_sensor[0] > 34 && baca_sensor[1] < 34 && 
     baca_sensor[2] > 34 && baca_sensor[3] > 34 && 
     baca_sensor[4] > 34 && baca_sensor[5] > 34){
+  digitalWrite (pinEnable, HIGH);
+digitalWrite (pinEnable2, HIGH);
+analogWrite (motor_in1, 0.2*255);
+analogWrite (motor_in2, 0);
+analogWrite (motor_on1, 0.5*255);
+analogWrite (motor_on2, 0);
   error = 1;
 }
   
-//jika hanya sensor 3 yang mendeteksi gelap, maka error terdeteksi
+//jika hanya sensor 3 yang mendeteksi gelap, maka error terdeteksi dan menjalankan perintah terdekat
 if (baca_sensor[0] > 34 && baca_sensor[1] > 34 && 
     baca_sensor[2] < 34 && baca_sensor[3] > 34 && 
     baca_sensor[4] > 34 && baca_sensor[5] > 34){
+  digitalWrite (pinEnable, HIGH);
+digitalWrite (pinEnable2, HIGH);
+analogWrite (motor_in1, 0.6*255);
+analogWrite (motor_in2, 0);
+analogWrite (motor_on1, 0.6*255);
+analogWrite (motor_on2, 0);
   error = 1;
+  
 }
   
-//jika hanya sensor 4 yang mendeteksi gelap, maka error terdeteksi
+//jika hanya sensor 4 yang mendeteksi gelap, maka error terdeteksi dan menjalankan perintah terdekat
 if (baca_sensor[0] > 34 && baca_sensor[1] > 34 && 
     baca_sensor[2] > 34 && baca_sensor[3] < 34 && 
     baca_sensor[4] > 34 && baca_sensor[5] > 34){
+  digitalWrite (pinEnable, HIGH);
+digitalWrite (pinEnable2, HIGH);
+analogWrite (motor_in1, 0.5*255);
+analogWrite (motor_in2, 0);
+analogWrite (motor_on1, 0.2*255);
+analogWrite (motor_on2, 0);
   error = 1;
 }
   
-//jika hanya sensor 5 yang mendeteksi gelap, maka error terdeteksi
+//jika hanya sensor 5 yang mendeteksi gelap, maka error terdeteksi dan menjalankan perintah terdekat
 if (baca_sensor[0] > 34 && baca_sensor[1] > 34 && 
     baca_sensor[2] > 34 && baca_sensor[3] > 34 && 
     baca_sensor[4] < 34 && baca_sensor[5] > 34){
   error = 1;
 }
   
-//jika hanya sensor 6 yang mendeteksi gelap, maka error terdeteksi
+//jika hanya sensor 6 yang mendeteksi gelap, maka error terdeteksi dan menjalankan perintah terdekat
 if (baca_sensor[0] > 34 && baca_sensor[1] > 34 && 
     baca_sensor[2] > 34 && baca_sensor[3] > 34 && 
     baca_sensor[4] > 34 && baca_sensor[5] < 34){
+  digitalWrite (pinEnable, HIGH);
+digitalWrite (pinEnable2, HIGH);
+analogWrite (motor_in1, 0.5*255);
+analogWrite (motor_in2, 0);
+analogWrite (motor_on1, 0);
+analogWrite (motor_on2, 0);
   error = 1;
 }
 }
